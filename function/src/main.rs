6a8d65121bc9da5783e3a2ba454f34758e9f5357
xiaoqinvar <17683860320m0@sina.cn>
2022-11-14 15:51:55 +0800

    learn: function

---
+++ function/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    // add(1, 1.1);
+    // decrement(10, 1);
+    // forever();
+    println!("{}", 1_i8 + 300);
+}
+// fn add(num1: i32, num2: f64) {
+//     println!("{}", num1);
+//     println!("{}", num2);
+// }
+
+// fn decrement(num1: i32, num2: i32) -> i32 {
+//     num1 - num2
+// }
+
+// fn forever() -> ! {
+//     loop {
+//         //...
+//     }
+// }
