9a8777c83ad9422bb21de8fb5e7f0254b0a21ddb
xiaoqinvar <17683860320m0@sina.cn>
2022-11-14 14:57:03 +0800

    learn: rust语句与表达式

--- char-bool-unit/src/main.rs
+++ char-bool-unit/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+// 字符类型
+/* fn main() {
+    let x = '🥶';
+    println!("rust字符占用字节大小：{}字节", std::mem::size_of_val(&x));
 }
+ */
+
+// 布尔类型
+/* fn main() {
+    let foo = true;
+    if foo {
+        println!("true");
+    }
+} */
+
+// 单元类型
+/* fn main() {
+    let unit: () = ();
+    assert_eq!(ret_fn(), unit);
+}
+fn ret_fn() {} */
+
+// 表达式
+fn main() {
+    let foo = {
+        let _x = 1;
+        1 + 1
+    };
+    println!("foo is {}", foo); // foo is 2
+    assert_eq!(test(), ()); // ✅
+}
+fn test() {}
